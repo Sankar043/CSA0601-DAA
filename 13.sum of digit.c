cea373917dc4722fc329af14c387e29b173ef14f
Sankar043 <112852342+Sankar043@users.noreply.github.com>
2023-08-26 10:17:44 +0530

    Update 13.sum of digit.c

--- 13.sum of digit.c
+++ 13.sum of digit.c
@@ -1,50 +1,14 @@
-#include <stdio.h>
-int sumOfDigits(int num) 
+#include<stdio.h>
+int  main()
 {
-    int sum = 0;
-    while (num > 0) 
+	int n;
+	printf("enter the number");
+	scanf("%d",&n);
+	int s=0;
+	while(n>0)
 	{
-        sum += num % 10;
-        num /= 10;
-    }
-    return sum;
-}
-int main() 
-{
-    int n,i,j;
-    printf("Enter the number of elements: ");
-    scanf("%d", &n);
-    if (n <= 1) 
-	{
-        printf("Invalid input.\n");
-        return 1;
-    }
-    int nums[n];
-    printf("Enter the positive integers:\n");
-    for (i = 0; i < n; i++) 
-	{
-        scanf("%d", &nums[i]);
-        if (nums[i] <= 0) 
-		{
-            printf("Invalid input.\n");
-            return 1;
-        }
-    }
-    int maxSum = 0;
-    for ( i = 0; i < n; i++) 
-	{
-        for ( j = i + 1; j < n; j++) 
-		{
-            if (sumOfDigits(nums[i]) == sumOfDigits(nums[j])) 
-			{
-                int currentSum = nums[i] + nums[j];
-                if (currentSum > maxSum) 
-				{
-                    maxSum = currentSum;
-                }
-            }
-        }
-    }
-    printf("Maximum sum with equal digit sums: %d\n", maxSum);
-    return 0;
+		s=s+n%10;
+		n=n/10;
+	}
+	printf("%d",s);
 }
